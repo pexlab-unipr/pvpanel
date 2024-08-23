436f1a4ea9efe3ac64e2064c743b35da90d83224
alessandro-soldati <alex89.ficus@gmail.com>
2024-08-23 15:59:00 +0200

    Completed first working draft of simulation function for panel with optimizers. Voltage and current limitations not enforced, just committing before adding them.

--- sim/compute_cell_res.m
+++ sim/compute_cell_res.m
@@ -1,4 +1,11 @@
 function out = compute_cell_res(vc, ic)
+    % If the input is a row or a column vector, reshape it to have the
+    % variable elements along the third dimension, thus being consistent
+    % with how cells array are managed
+    if isvector(vc)
+        vc = reshape(vc, [1, 1, numel(vc)]);
+        ic = reshape(ic, [1, 1, numel(ic)]);
+    end
     out.vc = vc;
     out.ic = ic;
     out.pc = vc.*ic;
--- sim/sim_panel_optimizers.m
+++ sim/sim_panel_optimizers.m
@@ -1,13 +1,29 @@
-function [outp, outc, outdb, outds] = sim_panel_optimizers(...
+function [outp, outc, outo] = sim_panel_optimizers(...
     par_sys, par_sim, par_cell)
     
+    Nsim = par_sim.Nsim;
+    Vmax = par_sim.Vmax;
+
     % Since micro-optimizers are assumed to replace bypass diodes, the
     % quantitative parameters of them are utilized to establish the
     % quantities
+    Ns = par_sys.Ns;
     Np = par_sys.Np; % number of parallels (of optimizer strings)
     Nops = par_sys.Nbdps; % number of optimizers per string
     Ncpo = par_sys.Ncpbd; % number of cells per optimizer
 
+    % Preallocate structure for cell working points: they are independent
+    % of the output voltage, because substrings are always kept in MPP by
+    % the optimizers
+    outc.v = zeros(Ns, Np);
+    outc.i = zeros(Ns, Np);
+    % Optimizer working points are different at the output, but unique at
+    % te input; on the output they do depend on overall panel voltage
+    outo.vi = zeros(Nops, Np);
+    outo.ii = zeros(Nops, Np);
+    outo.vo = zeros(Nops, Np, Nsim);
+    outo.io = zeros(Nops, Np, Nsim);
+    
     % 1) Compute curve of each substring, comprising Ncpbd cells in series
     for iis = 1:Np % for each string in parallel
         for iio = 1:Nops % for each optimizer/substring in string
@@ -17,19 +33,33 @@ function [outp, outc, outdb, outds] = sim_panel_optimizers(...
             this_par_sys.Ns = Ncpo;
             this_par_sys.Np = 1;
             this_par_sys.Nc = Ncpo;
-            [outp, outc] = sim_panel(this_par_sys, par_sim, this_par_cell);
+            [outss] = sim_panel(this_par_sys, par_sim, this_par_cell);
+
             % 2) Find MPP of each substring
-            outp
+            this_res = compute_cell_res(outss.vp, outss.ip);
+            outo.vi(iio,iis) = this_res.vmpp;
+            outo.ii(iio,iis) = this_res.impp;
+            % Equals for each optimizer input and all the cells connected
+            % to it
+            outc.v(((iio - 1)*Ncpo + 1):(iio*Ncpo),iis) = this_res.vmpp;
+            outc.i(((iio - 1)*Ncpo + 1):(iio*Ncpo),iis) = this_res.impp;
         end
     end
 
     % 3) Fix each converter output working point
-    
+    vp = reshape(linspace(0, Ns*Vmax, Nsim), [1 1 Nsim]);
+    pmpps = sum(outo.vi .* outo.ii, 1); % MPP power for each string
+    outo.vo = outo.vi .* outo.ii .* vp ./ pmpps;
+    outo.io = outo.vi .* outo.ii ./ outo.vo;
+    % Remove NaN or Inf arising dividing by zero (limits value)
+    flt = ...
+        isnan(outo.vo) | isinf(outo.vo) | ...
+        isnan(outo.io) | isinf(outo.io);
+    outo.vo(flt) = 0;
+    outo.io(flt) = 0;
+
     % 4) Compute the overall panel + optimizers characteristic curve
-    
-    % Dummy output, for now
-    outp = 0;
-    outc = 0;
-    outdb = 0;
-    outds = 0;
+    outp.v = vp(:);
+    outp.i = squeeze(sum(outo.io(1,:,:), 2));
+    outp.p = outp.v .* outp.i;
 end
