0ae0e8f4d5d40726e3f9d125770ca532323f0aa2
alessandro-soldati <alex89.ficus@gmail.com>
2025-11-04 18:10:08 +0100

    Characterization in resistance
    * Changed from current to resistance sweep
    * Log-spaced resistance values
    * Identified limitations in the range during LIST actuation
    * Tested on both PV and boost-based optimizer
    * Dependent values still to be optimized

--- sim/panel_emulation/panel_char.m
+++ sim/panel_emulation/panel_char.m
@@ -16,34 +16,36 @@ cmds = [
     "*CLS";
     "SYST:BEEP:STAT ON";
     "SYST:BEEP";
-    "FUNC CURR, (@1)";
+    "DISP:TEXT ""Ciao Alex!""";
+    "DISP:TEXT:CLE";
+    "FUNC RES, (@1)";
+    "RES 31, (@1)";
     "TRAN:MODE LIST, (@1)";
-    "LIST:CURR 0.5,1,2,3,4, (@1)";
-    "LIST:DWEL 1,1,1,1,1, (@1)";
-    "LIST:COUNT INF, (@1)";
+    "LIST:RES " + sprintf("%d,", logspace(log10(20), log10(1000), 100)) + " (@1)";
+    "LIST:DWEL 0.5, (@1)";
+    "LIST:COUNT 1, (@1)";
     "LIST:STEP AUTO, (@1)";
-    "CURR:MODE LIST, (@1)";
-    "TRIG:SOUR BUS";
+    "RES:MODE LIST, (@1)";
     "INIT:TRAN (@1)";
     "SENS:DLOG:FUNC:VOLT 1, (@1)";
     "SENS:DLOG:FUNC:CURR 1, (@1)";
-    "SENS:DLOG:TIME 30";
-    "SENS:DLOG:PER 0.2";
+    "SENS:DLOG:TIME 50";
+    "SENS:DLOG:PER 0.5";
     "TRIG:DLOG:SOUR BUS";
     "INIT:DLOG ""Internal:/log1.dlog""";
     "INP ON, (@1)";
     "*TRG"
 ];
 for ii = 1:length(cmds)
+    fprintf("%s\n", cmds(ii));
     writeline(inst, cmds(ii));
-    fprintf("%s\n", cmds(ii))
 end
 tim_wait = timer(...
     'StartFcn', @(~,~)disp('Waiting...'), ...
     'TimerFcn', @(~,~)disp('...'), ...
     'StopFcn', @(~,~)disp('Finished!'), ...
     'ExecutionMode', 'fixedRate', ...
-    'TasksToExecute', 35, ...
+    'TasksToExecute', 55, ...
     'Period', 1);
 start(tim_wait)
 wait(tim_wait)
@@ -51,7 +53,7 @@ writeline(inst, "INP OFF, (@1)")
 writeline(inst, "SYST:BEEP")
 
 %%
-data_str = writeread(inst, "FETC:DLOG? 150, (@1)");
+data_str = writeread(inst, "FETC:DLOG? 100, (@1)");
 data = str2num(data_str);
 data = reshape(data(:), numel(data)/2, 2);
 vs = data(:,1);
@@ -64,5 +66,19 @@ figure
 hold on
 plot(is, vs)
 plot(is, polyval(par, is))
+box on
+grid on
+%
+figure
+hold on
+plot(vs, is, '.-')
+xlabel('Voltage (V)')
+ylabel('Current (A)')
+box on
+grid on
+%
+figure
+hold on
+plot(vs, vs.*is, '.')
 box on
 grid on
