fcefa5496301a43e67d109fdbbb8857e717c288b
alessandro-soldati <alex89.ficus@gmail.com>
2026-08-20 14:16:50 +0200

    More simulation features
    * Automatic computation of MPP and use of it in case of null operating current
    * Possibility to specify non-unitary efficiency
    * Parametrized number of points
    * Computation of minimum output resistance for boost operation
    * General comments

--- sim/panel_emulation/optimizer_ideal.py
+++ sim/panel_emulation/optimizer_ideal.py
@@ -1,32 +1,77 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = ["numpy", "scipy", "matplotlib"]
+# ///
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
 
-Isc = 1.0
-Voc = 12.0
-Iset = 0.6
-a = 1.03
+# === PV description parameters ===
+Isc  = 1.0  # [A] PV panel short circuit current
+Voc  = 12.0 # [V] PV panel open circuit voltage
+Iset = 0.6  # [A] current working point (set to 0 for MPP)
+a    = 1.03 # [A] PV curve shape parameter (must be > Isc)
+RL   = 20   # [ohm] minimum output load resistance
+RH   = 1e3  # [ohm] maximum output load resistance
+eta  = 0.97 # [1] converter efficiency (assumed constant)
+Np   = 100  # [1] number of simulation evaluation points
 
-vi = np.linspace(0, Voc, 100)
+# === Simulation ===
+vi = np.linspace(0, Voc, Np)
+# Using model based on linear fractional function (homographic function)
 fpv = lambda vx: a*(vx - Voc) / (vx - a*Voc/Isc)
 ii = fpv(vi)
-Vset = spo.fsolve(lambda vx: fpv(vx) - Iset, Voc/2)[0]
-print(f"Vset: {Vset:.2f} V")
-Ro = np.logspace(np.log10(20), np.log10(1000), 100)
-vo = np.sqrt(Ro * Iset * Vset)
+# Look for MPP
+Vmpp = (spo.minimize_scalar(lambda vp: -vp*fpv(vp), bounds=(0, Voc), method='bounded')).x
+Impp = fpv(Vmpp)
+# In case of Iset == 0, use it
+if Iset < 1e-6:
+    (Vset, Iset) = (Vmpp, Impp)
+else:
+    # Find voltage at given input current
+    Vset = spo.fsolve(lambda vx: fpv(vx) - Iset, Voc/2)[0]
+# Sweep boost converter output resistance
+Rmin = Vset/Iset/eta # minimum output resistance to guarantee boost operation
+if RL < Rmin:
+    RL = Rmin
+Ro = np.logspace(np.log10(RL), np.log10(RH), Np)
+vo = np.sqrt(Ro * eta* Vset * Iset)
 io = vo / Ro
-duty = 1 - Vset/vo
+duty = 1 - Vset/vo # hypothesis of boost converter in CCM
+
+# === Results ===
+print(f"                 WP    |    MPP")
+print(f"Voltage (V)  : {Vset:5.2f}   |  {Vmpp:5.2f}")
+print(f"Current (A)  : {Iset:5.2f}   |  {Impp:5.2f}")
+print(f"Power   (W)  : {Vset*Iset:5.2f}   |  {Vmpp*Impp:5.2f}")
+print(f"Rmin    (ohm): {Rmin:5.2f}")
+print(f"Pout    (W)  : {eta*Vset*Iset:5.2f}")
 
 plt.figure()
-plt.plot(vi, ii)
+plt.plot(vi, ii, 'b-', label="PV characteristic")
+plt.plot(Vset, Iset, 'go', label="Working point")
+plt.plot(Vmpp, Impp, 'r*', label="MPP")
 plt.xlabel("Panel voltage (V)")
 plt.ylabel("Panel current (A)")
+plt.legend()
 plt.box(True)
 plt.grid(True)
 plt.show(block=False)
 
 plt.figure()
-plt.plot(vo, io)
+plt.plot(vi, vi*ii, 'b-', label="PV characteristic")
+plt.plot(Vset, Vset*Iset, 'go', label="Working point")
+plt.plot(Vmpp, Vmpp*Impp, 'r*', label="MPP")
+plt.xlabel("Panel voltage (V)")
+plt.ylabel("Panel power (W)")
+plt.legend()
+plt.box(True)
+plt.grid(True)
+plt.show(block=False)
+
+plt.figure()
+plt.plot(vo, io, 'b-')
 plt.xlabel("Output voltage (V)")
 plt.ylabel("Output current (A)")
 plt.box(True)
@@ -34,9 +79,11 @@ plt.grid(True)
 plt.show(block=False)
 
 plt.figure()
-plt.plot(Ro, duty)
+plt.plot(Ro, duty, 'b-')
+plt.plot([Rmin, Rmin], [0, 1], 'r--', label="Minimum resistance")
 plt.xlabel("Load resistance (Ohm)")
 plt.ylabel("Boost duty-cycle (1)")
+plt.legend()
 plt.box(True)
 plt.grid(True)
 plt.show()
