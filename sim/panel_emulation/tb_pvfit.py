0f628407390a61a5694938e1ed41a77f5132ea99
alessandro-soldati <alex89.ficus@gmail.com>
2026-09-01 18:07:12 +0200

    Major refactoring of interface
    * Removed inheritance, just one model class
    * Using common parameters, such as macimum power and temperature coefficients
    * Conditions as objects
    * Model as enum
    * Single function to plot both power and current

--- sim/panel_emulation/tb_pvfit.py
+++ sim/panel_emulation/tb_pvfit.py
@@ -3,86 +3,144 @@
 # dependencies = ["numpy", "scipy", "matplotlib", "pandas"]
 # ///
 
+from enum import Enum
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
+import scipy.interpolate as spi
 import pandas as pd
 
-class Pvmodel:
-    def __init__(self, name):
+# Support functions
+def extend(value, x, default=0):
+    y = np.broadcast_to( \
+        np.asarray(default if value is None else value), \
+        x.shape)
+    return y
+
+# PV condition class
+class PvCondition:
+    def __init__(self, irradiance, panel_temp, ambient_temp, air_mass, wind_speed):
+        self.irradiance = irradiance     # [W/m^2] panel irradiance
+        self.panel_temp = panel_temp     # [°C] panel temperature
+        self.ambient_temp = ambient_temp # [°C] ambient temperature
+        self.air_mass = air_mass         # [1] air mass
+        self.wind_speed = wind_speed     # [m/s] wind speed
+
+STC = PvCondition(irradiance=1000, panel_temp=25, ambient_temp=25, air_mass=1.5, wind_speed=0)
+NOCT = PvCondition(irradiance=800, panel_temp=45, ambient_temp=20, air_mass=1.5, wind_speed=1)
+
+# Enumeration for PV model
+PvModelType = Enum('PvModelType', [
+    ('TABLE'           , 1),
+    ('ELECTRIC'        , 2),
+    ('LINEAR_RATIONAL' , 3),
+    ('EXPONENTIAL'     , 4),
+    ('PIECEWISE_LINEAR', 5)])
+
+# Main PV model classes
+class PvModel:
+    def __init__(self, \
+                 voc, isc, \
+                 vmpp=None, impp=None, pmpp=None, ff=None, \
+                 ctc=0.05, vtc=-0.25, ptc=-0.35, \
+                 condition=STC, name=None, model=None):
+        self.voc = voc
+        self.isc = isc
+        self.vmpp = vmpp
+        self.impp = impp
+        self.pmpp = pmpp
+        self.ff = ff
+        self.ctc = ctc # [%/°C] short-circuit current temperature coefficient
+        self.vtc = vtc # [%/°C] open-circuit voltage temperature coefficient
+        self.ptc = ptc # [%/°C] maximum power temperature coefficient
+        self.condition = condition # condition at which data is represented, defaults to STC
         self.name = name
-        self.Voc = 0
-        self.Isc = 0
-        self.Vmpp = 0
-        self.Impp = 0
-        self.Pmpp = self.Vmpp * self.Impp
-        self.Idark = 0
-        self.FF = 0
-        self.Irr_norm = 1000 # [W/m^2]
-        self.T_norm = 25 # [°C]
-    def name(self):
-        return self.name
+        self.model = model
+        self.mpp_check()
+    def mpp_check(self):
+        # Assume similarity in ratio of MPP to characteristic boundaries
+        # TODO: check if similarity hypothesis hold
+        if (self.vmpp is not None) and (self.impp is not None): # MPP completely specified, overrides everything
+            self.pmpp = self.vmpp * self.impp
+            self.ff = self.pmpp/(self.voc * self.isc)
+        elif self.pmpp is not None: # maximum power specified, overrides fill factor
+            self.ff = self.pmpp/(self.voc * self.isc)
+            a = np.sqrt(self.ff)
+            self.vmpp = a * self.voc
+            self.impp = a * self.isc
+        elif self.ff is not None: # only fill factor given
+            self.pmpp = self.ff * self.voc * self.isc
+            a = np.sqrt(self.ff)
+            self.vmpp = a * self.voc
+            self.impp = a * self.isc
+        else:
+            raise ValueError("Either maximum power or fill factor must be specified.")
     def __str__(self):
         return (
             f"PV \"{self.name}\":\n"
-            f"    Voc  = {self.Voc:5.2f} V    Isc  = {self.Isc:5.2f} A    FF   = {self.FF*100:4.2f} %\n"
-            f"    Vmpp = {self.Vmpp:5.2f} V    Impp = {self.Impp:5.2f} A    Pmpp = {self.Pmpp:5.2f} W"
+            f"    Voc  = {self.voc:5.2f} V    Isc  = {self.isc:5.2f} A    FF   = {self.ff*100:4.2f} %\n"
+            f"    Vmpp = {self.vmpp:5.2f} V    Impp = {self.impp:5.2f} A    Pmpp = {self.pmpp:5.2f} W"
         )
-    def mpp(self):
-        return self.Vmpp, self.Impp, self.Pmpp, self.FF
-    def current(self, vp, irr=None, Tp=None):
-        if irr is None:
-            irr = self.Irr_norm
-        if Tp is None:
-            Tp = self.T_norm
-        # TODO: manage temperature in code, not only in interface
-        return self.Isc * irr/self.Irr_norm * np.ones_like(vp) # dummy values
-    def power(self, vp, irr=None, Tp=None):
-        return vp * self.current(vp, irr, Tp)
-    def plot(self, block=True, Npts=100, irr=None, Tp=None):
-        vp = np.linspace(0, self.Voc, Npts)
+    def current_model(self, vp, voc, isc, vmpp, impp, pmpp, model):
+        match model:
+            case PvModelType.TABLE:
+                ip = isc * np.ones_like(vp)
+            case PvModelType.ELECTRIC:
+                ip = isc * np.ones_like(vp)
+            case PvModelType.LINEAR_RATIONAL:
+                ip = isc * np.ones_like(vp)
+            case PvModelType.EXPONENTIAL:
+                ip = isc * np.ones_like(vp)
+            case PvModelType.PIECEWISE_LINEAR:
+                # ip = np.interp(vp, [0, vmpp, voc], [isc, impp, 0]) # does not extrapolate!!
+                ip = spi.make_interp_spline([0, vmpp, voc], [isc, impp, 0], k=1)(vp)
+            case _:
+                raise ValueError("Unknown or unspecified PV model type.")
+        return ip
+    def current(self, vp, condition=STC, model=None):
+        # Scale short-circuit current in irradiance and temperature
+        isc = self.isc * (condition.irradiance/self.condition.irradiance) * (1 + self.ctc/100 * (condition.panel_temp - self.condition.panel_temp))
+        # Scale open-circuit voltage in temperature only
+        voc = self.voc * (1 + self.vtc/100 * (condition.panel_temp - self.condition.panel_temp))
+        # TODO: check how Vmpp and Impp scale with temperature
+        # For now, assuming they scale as Voc and Isc, respectively
+        impp = self.impp * (condition.irradiance/self.condition.irradiance) * (1 + self.ctc/100 * (condition.panel_temp - self.condition.panel_temp))
+        vmpp = self.vmpp * (1 + self.vtc/100 * (condition.panel_temp - self.condition.panel_temp))
+        # Scale power in case it is needed by the interpolation model
+        pmpp = self.pmpp * (1 + self.ptc/100 * (condition.panel_temp - self.condition.panel_temp))
+        # Compute current according to model
+        model = self.model if model is None else model
+        # TODO: check how to pass data at the specific current model
+        ip = self.current_model(vp, voc, isc, vmpp, impp, pmpp, model)
+        return ip
+    def power(self, vp, condition=STC, model=None):
+        return vp * self.current(vp, condition, model)
+    def mpp(self, condition=STC):
+        # TODO: recompute values according to condition and model
+        return self.vmpp, self.impp, self.pmpp, self.FF
+    def plot(self, \
+             conditions=[STC], model=None, \
+             plot_current=True, plot_power=False, plot_mpp=True, \
+             block=True, Npts=100):
+        vp = np.linspace(0, self.voc, Npts)
+        model = self.model if model is None else model
+        x = np.ones_like(vp) if plot_current else vp
         plt.figure()
-        if irr is None:
-            irr = self.Irr_norm
-        if Tp is None:
-            Tp = self.T_norm
-        conditions = np.hstack((np.reshape(irr, (-1, 1)), np.reshape(Tp, (-1, 1))))
         for condition in conditions:
-            (irri, Tpi) = condition
-            ip = self.current(vp, irri, Tpi)
-            plt.plot(vp, ip, 'b-')
-        plt.plot(vp, self.current(vp), 'g--', label="normal")
-        plt.plot(self.Vmpp, self.Impp, 'r*', label="MPP")
+            ip = self.current(vp, condition, model)
+            plt.plot(vp, x * ip, 'b-')
+        plt.plot(vp, x * self.current(vp, condition=STC, model=model), 'g--', label="STC")
+        plt.plot(vp, x * self.current(vp, condition=NOCT, model=model), 'm--', label="NOCT")
+        plt.plot(self.vmpp, self.impp, 'r*', label="MPP")
         plt.xlabel("Output voltage (V)")
-        plt.ylabel("Output current (A)")
-        plt.xlim(0, self.Voc)
-        plt.ylim(0, self.Isc*1.1)
-        plt.box(True)
-        plt.grid(True)
-        plt.show(block=block)
-    def plot_power(self, block=True, Npts=100, irr=None, Tp=None):
-        vp = np.linspace(0, self.Voc, Npts)
-        plt.figure()
-        if irr is None:
-            irr = self.Irr_norm
-        if Tp is None:
-            Tp = self.T_norm
-        conditions = np.hstack((np.reshape(irr, (-1, 1)), np.reshape(Tp, (-1, 1))))
-        for condition in conditions:
-            (irri, Tpi) = condition
-            ip = self.current(vp, irri, Tpi)
-            plt.plot(vp, vp * ip, 'b-')
-        plt.plot(vp, vp * self.current(vp), 'g--', label="normal")
-        plt.plot(self.Vmpp, self.Pmpp, 'r*', label="MPP")
-        plt.xlabel("Output voltage (V)")
-        plt.ylabel("Output power (W)")
-        plt.xlim(0, self.Voc)
-        plt.ylim(0, self.Pmpp*1.1)
+        plt.ylabel("Output current (A)") # plt.ylabel("Output power (W)")
+        plt.xlim(0, self.voc)
+        plt.ylim(0, self.isc * 1.1) # plt.ylim(0, self.Pmpp * 1.1)
         plt.box(True)
         plt.grid(True)
         plt.show(block=block)
 
-class Pvmodel_table(Pvmodel):
+""" class Pvmodel_table(Pvmodel):
     def __init__(self, name, filename):
         super().__init__(name)
         data = pd.read_csv(filename, sep=',', header=0, usecols=["V", "I"])
@@ -187,41 +245,21 @@ class Pvmodel_pwl(Pvmodel):
         ip_doff = self.Isc * irr/self.Irr_norm - 1/self.Rp * vp
         ip_don = (self.Voc - vp)/self.Rs
         ip = np.minimum(ip_doff, ip_don)
-        return ip
+        return ip """
 
 # ==== Test bench ====
 
-pva = Pvmodel_rational("dummy analytical", 14, 8, 8.1)
-print(pva)
-pva.plot(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
-pva.plot_power(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
-# pva.plot(False)
-# pva.plot_power(False)
-
-pvt = Pvmodel_table("openei table", "10333_34_5_01152020.csv")
-print(pvt)
-pvt.plot(False)
-pvt.plot_power(False)
-pvt.plot(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
-pvt.plot_power(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
-
-Ias = np.linspace(8.01, 9, 100)
-FFs = np.array([Pvmodel_rational("asd", 14, 8, Ia).mpp()[3] for Ia in Ias])
-plt.figure()
-plt.plot(Ias, FFs)
-plt.show(block=False)
-
-pvf = Pvmodel_rational.from_data("openei fitted", "10333_34_5_01152020.csv")
-print(pvf)
-pvf.plot(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
-
-pve = Pvmodel_electric("elec", 14, 8, 1e-9, 1.4, 0.1, 1)
-print(pve)
-pve.plot(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
-
-pvp = Pvmodel_pwl("pwl", 14, 8, 0.1, 30)
-print(pvp)
-pvp.plot(False, irr=np.array([300, 600, 900]), Tp=np.array([40, 40, 40]))
+pv1 = PvModel(48, 12, ff=0.75)
+pv2 = PvModel(48, 12, ff=0.75, name="prova")
+pv3 = PvModel(48, 12, pmpp=400)
+pv4 = PvModel(48, 12, pmpp=400, ff=0.8)
+pv5 = PvModel(48, 12, vmpp=44, impp=9)
+print(pv1)
+print(pv2)
+print(pv3)
+print(pv4)
+print(pv5)
+pv1.plot(model=PvModelType.PIECEWISE_LINEAR)
 
 print("Ciao!")
 plt.close('all')
