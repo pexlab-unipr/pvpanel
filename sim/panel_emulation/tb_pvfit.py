f8a5b8627a71ab15336457184c1b8dc0ea3ef012
alessandro-soldati <alex89.ficus@gmail.com>
2026-09-08 22:58:54 +0200

    Four parameters and four degrees of freedom
    * Tested only on linear rational model
    * Full solution of system, converges but sometimes it fails

--- sim/panel_emulation/tb_pvfit.py
+++ sim/panel_emulation/tb_pvfit.py
@@ -112,9 +112,26 @@ class PvModel:
             case PvModelType.ELECTRIC:
                 ip = isc * np.ones_like(vp)
             case PvModelType.LINEAR_RATIONAL:
-                ff = pmpp/(voc*isc)
-                Ia = isc*ff/(2*np.sqrt(ff) - 1)
-                ip = Ia * (vp - voc)/(vp - voc/isc*Ia)
+                # ff = pmpp/(voc*isc)
+                # Ia = isc*ff/(2*np.sqrt(ff) - 1)
+                # ip = Ia * (vp - voc)/(vp - voc/isc*Ia)
+                mdl = lambda x, a, b, c, d: \
+                    a + b*x + c/(x + d)
+                mdlp = lambda x, a, b, c, d: \
+                    b - c/(x + d)**2
+                fun = lambda y: [
+                    mdl(0, y[0], y[1],y[2], y[3]) - isc,
+                    mdl(voc, y[0], y[1],y[2], y[3]),
+                    mdl(vmpp, y[0], y[1],y[2], y[3]) - impp,
+                    mdlp(vmpp, y[0], y[1],y[2], y[3]) + impp/vmpp
+                ]
+                y0 = [isc, -0.1, 1, -1.05*voc]
+                sol = spo.root(fun, y0)
+                y = sol.x
+                print(y)
+                print(sol.message)
+                a, b, c, d = y
+                ip = mdl(vp, a, b, c, d)
             case PvModelType.EXPONENTIAL:
                 mdl = lambda vx, p: p[0] + p[1] * np.exp(vx/p[2])
                 mdlp = lambda vx, p: p[1]/p[2] * np.exp(vx/p[2])
